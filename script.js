608aee3029eb8bff0d4a94647e7ace0e27bded91
Ant0n <tetrips@gmail.com>
2025-12-24 17:56:17 +0100

    ok manque clean

--- script.js
+++ script.js
@@ -12,11 +12,7 @@ function operator(a, b, operator) {
         case '-':
             return subtract(a, b);
         case '*':
-            if(roundRobin == 0) {
-            return multiply(1, b);
-            } else {
             return multiply(a, b);
-            }
         case '/':
             return divide(a, b);
     };
@@ -33,37 +29,55 @@ function populateScreen() {
     let clearSelector = document.querySelector('#clear');
 
     numberSelector.addEventListener('click', function(event) {
-
-        buffer.push(event.target.textContent)
-        console.log(`buffer modifié : ${buffer}`);
-
+        let chiffre = event.target.textContent;
+        if (["+","-","/","*"].includes(buffer.at(-1))) {
+            screenSelector.innerHTML = '';
+        }
+        buffer.push(chiffre);
+        screenSelector.innerHTML += chiffre; 
         })
 
     signSelector.addEventListener('click', function(event) {
-
-    if (buffer.includes("+") || buffer.includes("-") || buffer.includes("/")) {
-    
-        console.log(`Un signe dans le buffer existe déja. Récupération des deux côtés du buffer : ${buffer}`)
+    if (event.target.textContent == "=") {
+        console.log('========== event.target.textContent =========')
         let indexSign = buffer.indexOf(currentSign);
-
         let premierePartie = buffer.slice(0, indexSign).join('');
-
         let deuxiemePartie = buffer.slice(indexSign+1, buffer.length).join('');
-
         result = operator(premierePartie, deuxiemePartie, currentSign)
+
         buffer = [];
         buffer.push(result);
+        console.log(`result: ${result}`);
+        console.log(`buffer: ${buffer}`);
+        screenSelector.innerHTML = result;
 
-        console.log(`result = ${result}`)
+
+    } else if (event.target.textContent != "="){
+        if (buffer.includes("+") || buffer.includes("-") || buffer.includes("/") || buffer.includes("*")) {
+    
+            console.log(`Un signe dans le buffer existe déja. Récupération des deux côtés du buffer : ${buffer}`)
+
+            let indexSign = buffer.indexOf(currentSign);
+
+            let premierePartie = buffer.slice(0, indexSign).join('');
+
+            let deuxiemePartie = buffer.slice(indexSign+1, buffer.length).join('');
+
+            result = operator(premierePartie, deuxiemePartie, currentSign)
+            buffer = [];
+            buffer.push(result);
+            buffer.push(event.target.textContent);
+
+            console.log(`result = ${result}`)
         // buffer.reduce( (result, [buffer.splice(indexOf(currentSign))]) => )
-        currentSign = event.target.textContent;
-    } else {
-        buffer.push(event.target.textContent);
-        currentSign = event.target.textContent;
-        console.log(`Aucun signe dans le buffer n'existe. buffer : ${buffer}`)
-        console.log(`Aucun signe dans le buffer n'existe. currentSign : ${currentSign}`)
+            currentSign = event.target.textContent;
+        } else {
+            buffer.push(event.target.textContent);
+            currentSign = event.target.textContent;
+            console.log(`Aucun signe dans le buffer n'existe. buffer : ${buffer}`)
+            console.log(`Aucun signe dans le buffer n'existe. currentSign : ${currentSign}`)
+        }
     }
-
 })
 
 }
