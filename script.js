65949e64426bbd43e5b94813e3ebd92611c98181
Ant0n-Work <tetrips@gmail.com>
2025-12-26 10:36:34 +0100

    calculette terminée

--- script.js
+++ script.js
@@ -29,6 +29,13 @@ function populateScreen() {
     let screenSelector = document.querySelector('.result');
     let clearSelector = document.querySelector('#clear');
 
+    clearSelector.addEventListener('click', function(event) {
+        screenSelector.innerHTML = '0000'
+        currentSign = ''
+        buffer = [];
+        result = '';
+        start = 0;
+        })
 
     numberSelector.addEventListener('click', function(event) {
         if (start == 0) {
@@ -44,8 +51,9 @@ function populateScreen() {
         })
 
     signSelector.addEventListener('click', function(event) {
+
     if (event.target.textContent == "=") {
-        console.log('========== event.target.textContent =========')
+        console.log('########### event.target.textContent = "=" #############')
         let indexSign = buffer.indexOf(currentSign);
         let premierePartie = buffer.slice(0, indexSign).join('');
         let deuxiemePartie = buffer.slice(indexSign+1, buffer.length).join('');
@@ -59,29 +67,32 @@ function populateScreen() {
 
 
     } else if (event.target.textContent != "="){
-        if (buffer.includes("+") || buffer.includes("-") || buffer.includes("/") || buffer.includes("*")) {
-    
-            console.log(`Un signe dans le buffer existe déja. Récupération des deux côtés du buffer : ${buffer}`)
-
+        // Si un signe existe dans le buffer et qu'il n'est pas son dernier élément,
+        // on coupe le buffer en 2 par le signe, puis on fait une opération entre ses deux partie. Le résultat sera alors stocké
+        // dans la premiere partie du buffer.
+        if ((buffer.includes("+") || buffer.includes("-") || buffer.includes("/") || buffer.includes("*")) && !(["+","-","/","*"].includes(buffer.at(-1)))) {
             let indexSign = buffer.indexOf(currentSign);
-
             let premierePartie = buffer.slice(0, indexSign).join('');
-
             let deuxiemePartie = buffer.slice(indexSign+1, buffer.length).join('');
-
             result = operator(premierePartie, deuxiemePartie, currentSign)
+            // reset du buffer ->  push du résultat -> stockage du signe courant (dernier signe appuyé).
             buffer = [];
             buffer.push(result);
             buffer.push(event.target.textContent);
-
-            console.log(`result = ${result}`)
-        // buffer.reduce( (result, [buffer.splice(indexOf(currentSign))]) => )
+            screenSelector.innerHTML = result;
             currentSign = event.target.textContent;
         } else {
+            // Si on appuie deux fois de suite sur un signe, le dernier signe est remplacé par le nouveau signe.
+            if (["+","-","/","*"].includes(buffer.at(-1))) {
+                buffer[buffer.length - 1] = event.target.textContent;
+                currentSign = event.target.textContent;
+            } else {
+            // Aucun signe dans le buffer n'existe : On push le sign dans le buffer et on stock la variable dans "currentSign"
             buffer.push(event.target.textContent);
             currentSign = event.target.textContent;
             console.log(`Aucun signe dans le buffer n'existe. buffer : ${buffer}`)
             console.log(`Aucun signe dans le buffer n'existe. currentSign : ${currentSign}`)
+            }
         }
     }
 })
